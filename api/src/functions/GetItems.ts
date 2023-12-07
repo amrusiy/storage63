ab039ace17e2dceb840cc61a81c24bc6ab935815
amrusiy <amrusi.yarin@gmail.com>
2023-12-07 22:31:59 +0200

    all project

--- api/src/functions/GetItems.ts
+++ api/src/functions/GetItems.ts
@@ -4,21 +4,55 @@ import { CosmosClient } from "@azure/cosmos";
 app.http("GetItems", {
   methods: ["GET"],
   authLevel: "anonymous",
-  handler: async () => {
-    const cosmosClient = new CosmosClient(
-      process.env.COSMOSDB_CONNECTION_STRING
-    );
-    const { resources: items } = await cosmosClient
-      .database("db")
-      .container("items")
-      .items.readAll()
-      .fetchAll();
-
+  route: "items/{id?}",
+  handler: async (request, context) => {
     try {
-      return {
-        status: 200,
-        body: JSON.stringify(items),
-      };
+      const connectionString = process.env.COSMOSDB_CONNECTION_STRING;
+      const cosmosClient = new CosmosClient(connectionString);
+
+      async function getChildUnitIds(unitId: string): Promise<string[]> {
+        const {
+          resource: { childUnitIds },
+        } = await cosmosClient
+          .database("db")
+          .container("units")
+          .item(unitId)
+          .read<{ childUnitIds: string[] }>();
+        const childUnits = await (
+          await Promise.all(
+            childUnitIds.map(async (unitId) => await getChildUnitIds(unitId))
+          )
+        ).flat();
+        return [unitId, ...childUnits];
+      }
+
+      // authorization
+      const userId = request.headers.get("userId");
+      const password = request.headers.get("password");
+      const { resource: user } = await cosmosClient
+        .database("db")
+        .container("users")
+        .item(userId)
+        .read();
+      if (!user) return { status: 401 };
+      if (user.password !== password) return { status: 401 };
+
+      if (request.params.id) {
+        // Get specific item
+      } else {
+        // Get list of items
+        const unitIds = getChildUnitIds(user.unitId);
+
+        const { resources: items } = await cosmosClient
+          .database("db")
+          .container("items")
+          .items.query("SELECT id,sku,status FROM items")
+          .fetchAll();
+        return {
+          status: 200,
+          body: JSON.stringify(items),
+        };
+      }
     } catch (error) {
       return {
         status: 500,
@@ -26,5 +60,4 @@ app.http("GetItems", {
       };
     }
   },
-  route: "items",
 });
