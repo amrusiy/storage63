37c2861a0b3001eae1a714206d4668400ec2ac27
amrusiy <amrusi.yarin@gmail.com>
2023-12-24 16:28:27 +0200

    update GetItems add context.log

--- api/src/functions/items/GetItems.ts
+++ api/src/functions/items/GetItems.ts
@@ -13,6 +13,7 @@ app.http("GetItems", {
       const connectionString = process.env.COSMOSDB_CONNECTION_STRING;
       const cosmos = new CosmosClient(connectionString);
       const user = await authenticate(request);
+      context.log(user);
 
       if (request.params.id) {
         // Get specific item
@@ -23,37 +24,43 @@ app.http("GetItems", {
           .read();
         return {
           status: item ? 200 : 404,
-          body: item ? JSON.stringify(item) : `item with the id ${request.params.id} was not found.`
-        }
+          body: item
+            ? JSON.stringify(item)
+            : `item with the id ${request.params.id} was not found.`,
+        };
       } else {
         // Get list of items
         const unitIds = await getChildUnitIds(user.unitId);
-        const filter = ['unitId', 'sku', 'status'].map(key => ` AND ${key} = ${request.query.get(key)}`).join('');
+        const filter = ["unitId", "sku", "status"]
+          .map((key) => ` AND ${key} = ${request.query.get(key)}`)
+          .join("");
         const order = request.query.get("orderBy");
         const group = request.query.get("groupBy");
         const { resources: items } = await cosmos
           .database("db")
           .container("items")
-          .items.query(`SELECT i.id, i.sku, i.status, i.unitId FROM items i WHERE i.unitId IN (${unitIds.map(id => `"${id}"`).join()})${filter}ORDER BY ${order} ASC`)
+          .items.query(
+            `SELECT i.id, i.sku, i.status, i.unitId FROM items i WHERE i.unitId IN (${unitIds
+              .map((id) => `"${id}"`)
+              .join()})${filter}ORDER BY ${order} ASC`
+          )
           .fetchAll();
         return {
           status: 200,
-          body: JSON.stringify(group
-            ? Object
-              .entries(groupBy(items, group))
-              .map(_ => ({
+          body: JSON.stringify(
+            group
+              ? Object.entries(groupBy(items, group)).map((_) => ({
                   [group]: _[0],
-                  items: _[1]
-              })
-            )
-            : items
+                  items: _[1],
+                }))
+              : items
           ),
         };
       }
     } catch (error) {
       return {
         status: error.status ?? 500,
-        body: error.message
+        body: error.message,
       };
     }
   },
