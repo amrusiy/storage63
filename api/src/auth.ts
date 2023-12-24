9e1773516bb3187fec4b43d4794701aa3c43cbe5
amrusiy <amrusi.yarin@gmail.com>
2023-12-24 16:19:57 +0200

    update auth change userId to username

--- api/src/auth.ts
+++ api/src/auth.ts
@@ -2,19 +2,20 @@ import { CosmosClient } from "@azure/cosmos";
 import { HttpRequest } from "@azure/functions";
 
 export async function authenticate(request: HttpRequest) {
-    const connectionString = process.env.COSMOSDB_CONNECTION_STRING;
-    const cosmosClient = new CosmosClient(connectionString);
+  const connectionString = process.env.COSMOSDB_CONNECTION_STRING;
+  const cosmosClient = new CosmosClient(connectionString);
 
-    const userId = request.headers.get("userId");
-    const password = request.headers.get("password");
+  const password = request.headers.get("password");
+  const username = request.headers.get("username");
 
-    if (!userId || !password) throw { status: 401 }
+  if (!username || !password) throw { status: 401 };
 
-    const { resource: user } = await cosmosClient
-        .database("db")
-        .container("users")
-        .item(userId, userId)
-        .read();
-    if (!user || user.password !== password) throw { status: 401 };
-    return user;
+  const { resource: user } = await cosmosClient
+    .database("db")
+    .container("users")
+    .item(username, username)
+    .read();
+  if (!user || user.username !== username || user.password !== password)
+    throw { status: 401 };
+  return user;
 }
